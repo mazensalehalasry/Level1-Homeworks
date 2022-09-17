12d33f49acc9c98c3423026e00d74032d1351d50
Mazen Saleh Al-Asry <113477388+mazensalehalasry@users.noreply.github.com>
2022-09-18 00:50:37 +0300

    Add files via upload

---
+++ Day_2_JS_homework/js/script.js
@@ -0,0 +1,7 @@
+alert("Day 2 JS homework");
+let x = 10;     //  Assigning the value 10 to variable x 
+let y = 12;     //  Assigning the value 12 to variable y
+let z = x + y;  //  Assigning the sum of x value & y value to variable z
+
+console.log(z); //   The z value displayed in the console
+
---
+++ Day_3_JS_homework_dogage/js/script.js
@@ -0,0 +1,16 @@
+
+function calculateDogAge(){
+
+    let dog = {
+        "name": "OTIS",
+        "age": 5,
+        "family": 'bulldog',
+        "ispit": true,
+    }
+	return dog.age;
+}
+    let dogYears = 7 * calculateDogAge();
+    
+
+    console.log(`Your doggie is ${dogYears} years old in dog years!`);
+
